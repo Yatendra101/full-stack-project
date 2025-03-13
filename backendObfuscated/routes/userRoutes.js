cdf51c56ec9244f3e5062d4300a2012e53620f5b
Yatendra101 <yatendras0@gmail.com>
2025-03-13 15:09:17 +0530

    Update userRoutes.js

--- backendObfuscated/routes/userRoutes.js
+++ backendObfuscated/routes/userRoutes.js
@@ -39,7 +39,7 @@ router.route('/me')
 
 // Booking Routes
 router.route('/booking')
-    .get(authController.protect, bookingController.getcheckOutSession);
+    .get(authController.protect, bookingController.getCheckOutSession);
 
 router.route('/booking/:bookingId')
     .patch(authController.protect, bookingController.getBookingDetails);
