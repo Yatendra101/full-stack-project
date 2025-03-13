16a38ff9ba00d9acf8bbbd43153f94f296357f91
Yatendra101 <yatendras0@gmail.com>
2025-03-13 09:38:02 +0530

    Update propertyRoutes.js

--- backendObfuscated/routes/propertyRoutes.js
+++ backendObfuscated/routes/propertyRoutes.js
@@ -16,42 +16,12 @@ router.get("/listing", getProperties);
 router.get("/:id", getProperty);
 
 // Route to create a new property
-router.post("/", async (req, res) => {
-  try {
-    const newProperty = await createProperty(req.body);
-    res.status(201).json(newProperty);
-  } catch (error) {
-    console.error("Error creating property:", error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
-});
+router.post("/", createProperty);
 
 // Route to update a property by ID
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedProperty = await updateProperty(req.params.id, req.body);
-    if (!updatedProperty) {
-      return res.status(404).json({ message: "Property not found" });
-    }
-    res.status(200).json(updatedProperty);
-  } catch (error) {
-    console.error("Error updating property:", error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
-});
+router.put("/:id", updateProperty);
 
 // Route to delete a property by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const deletedProperty = await deleteProperty(req.params.id);
-    if (!deletedProperty) {
-      return res.status(404).json({ message: "Property not found" });
-    }
-    res.status(200).json({ message: "Property deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting property:", error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
-});
+router.delete("/:id", deleteProperty);
 
 export default router;
