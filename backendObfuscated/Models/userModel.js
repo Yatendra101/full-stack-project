5a355b21b84295cedc324bb0d8175c83dbaabee9
Yatendra101 <yatendras0@gmail.com>
2025-03-13 14:05:03 +0530

    Update userModel.js

--- backendObfuscated/Models/userModel.js
+++ backendObfuscated/Models/userModel.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+
 
 const userSchema = new mongoose.Schema(
     {
@@ -80,4 +81,5 @@ userSchema.methods.createPasswordResetToken = function () {
 };
 
 const User = mongoose.model('User', userSchema);
-module.exports = User;
+export default User;
+
